82c833f9f2082150fe6896d430ff63d3a1654374
Marko <vukovic.marko171@gmail.com>
2019-05-25 23:15:01 +0200

    Implementirana forma za pretragu automobila

---
+++ frontend/src/components/FilterCarsSlot.vue
@@ -0,0 +1,86 @@
+<template>
+    <div>    
+        <form v-on:submit="sub">
+            <div class="form-row">
+                <div class="form-group col-md-4">
+                    <input type="text" class="form-control" id="modelAutomobila" placeholder="Model automobila">
+                </div>
+                <div class="form-group col-md-1">
+
+                </div>
+                <div class="form-group col-md-2">
+                    <input type="number" class="form-control" id="minPrice" placeholder="Cena od" min="1">
+                </div>
+                <div class="form-group col-md-2">
+                    <input type="number" class="form-control" id="maxPrice" placeholder="Do" min="1">
+                </div>
+                <div class="form-group col-md-1">
+
+                </div>
+                <div class="form-group col-md-2">
+                    <select class="form-control" id="vrstaGorivaAutomobila">
+                        <option>Dizel</option>
+                        <option>Benzin</option>
+                    </select>
+                </div>
+            </div>
+            <div class="form-row">
+                
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-2">
+                    <input type="number" class="form-control" id="minKubikaza" placeholder="Kubikaža od" min="1">
+                </div>
+                <div class="form-group col-md-2">
+                    <input type="number" class="form-control" id="maxKubikaza" placeholder="Do" min="1">
+                </div>
+                <div class="form-group col-md-1">
+
+                </div>
+                <div class="form-group col-md-2">
+                    <input type="number" class="form-control" id="minGodiste" placeholder="Godište od" min="1950">
+                </div>
+                <div class="form-group col-md-2">
+                    <input type="number" class="form-control" id="maxGodiste" placeholder="Do" max="2019">
+                </div>
+                <div class="form-group col-md-1">
+
+                </div>
+                
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-4">
+                    <input type="text" class="form-control" id="brojSedista" placeholder="Broj sedišta" min="1">
+                </div>
+                <div class="form-group col-md-4">
+                    <input type="text" class="form-control" id="brojVrata" placeholder="Broj vrata" min="1">
+                </div>
+                <div class="form-group col-md-4">
+                    <input type="text" class="form-control" id="bojaAutomobila" placeholder="Boja">
+                </div>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Pretraži</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'filterCarsSlot',
+    components: {
+
+    },
+    methods: {
+        sub: function(e) {
+            e.preventDefault();
+            
+            alert("form submited!");
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- frontend/src/views/Explore.vue
+++ frontend/src/views/Explore.vue
@@ -1,5 +1,32 @@
 <template>
   <div class="explore">
-    <h1>This is an explore page</h1>
+    <h1>Pretraga automobila</h1>
+    <FilterCarsSlot id="filterCarsSlot" />
   </div>
 </template>
+
+<script>
+
+import FilterCarsSlot from '@/components/FilterCarsSlot.vue'
+
+
+export default {
+  components: {
+    FilterCarsSlot
+  }
+}
+</script>
+
+<style>
+h1 {
+  margin: 20px 0px 20px;
+}
+#filterCarsSlot {
+  position: fixed;
+  height: 40%;
+  width: 100%;
+  padding: 0% 10% 0%;
+  overflow-y: scroll;
+}
+</style>
+
